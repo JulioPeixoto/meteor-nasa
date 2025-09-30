b0af8efc0ba36c363608ef3af9791f6685e3b6f5
JulioPeixoto <juliorafaelnft@hotmail.com>
2025-09-30 01:06:56 -0300

    feat: configuração inicial do projeto com React, Next.js e Tailwind

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.styl': {
-          loaders: ['@turbo/loader-stylus'],
-          as: '*.css',
-        },
-      },
-    },
-  },
+  reactStrictMode: true,
 }
 
 export default nextConfig
